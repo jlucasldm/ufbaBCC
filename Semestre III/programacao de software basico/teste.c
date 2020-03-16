1e714f826cfb2512684db95c89597b83a33d8929
João Lucas <jlucas.ldm@gmail.com>
2020-03-16 15:52:23 -0300

    Add files via upload

---
+++ Semestre III/programacao de software basico/teste.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+	float numero;
+	int vetor[16];
+	int i = 0;
+	int j;
+	
+	scanf("%f", &numero);
+
+	while(numero != 0){
+		if(numero<1){
+			numero*=2;
+			if(numero<1){
+				vetor[i]=0;			
+			}else if(numero>=1){
+				vetor[i]=1;
+				numero-=1;			
+			}		
+		}
+		i++;
+		j=i;
+	}
+
+	printf("0.");
+	for(i=0; i<j; i++){
+		printf("%d", vetor[i]);
+	}
+	printf("\n");	
+	return 0;
+}
