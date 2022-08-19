a3561aa55e199cf5122c2e525f365fc47f510591
jlucasldm <jlucas.ldm@gmail.com>
2022-08-19 18:35:33 -0300

    cleaning folders

--- 2022.1/sistemas_operacionais/projeto_final/simulador-processos-memoria-final/typical-rest-server-forked/database/pg/seed/book.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO books (id, title, author) VALUES (1, 'Herman Melville', 'Moby Dick');
-INSERT INTO books (id, title, author) VALUES (2, 'Leo Tolstoy', 'War and Peace');
-INSERT INTO books (id, title, author) VALUES (3, 'William Shakespeare', 'Hamlet');
-INSERT INTO books (id, title, author) VALUES (4, 'Homer', 'The Odyssey');
-INSERT INTO books (id, title, author) VALUES (5, 'Mark Twain', 'The Adventures of Huckleberry Finn');
-
-SELECT setval('books_id_seq', (SELECT MAX(id) FROM books));
--- 2022.1/sistemas_operacionais/projeto_final/simulador-processos-memoria-final/typical-rest-server-forked/pkg/echokit/echokit.go
+++
@@ -1,6 +0,0 @@
-package echokit
-
-const (
-	// HeaderTotalCount header total count
-	HeaderTotalCount = "X-Total-Count"
-)
